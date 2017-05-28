61d4f9151d5c72feb09c0cfd7c73b7725cb3d622
Mehmetjan <mbbugra@gmail.com>
2017-05-27 22:31:11 -0400

    test but not ready

--- NYT/src/test/java/testHomePage/TestShowWeather.java
+++ NYT/src/test/java/testHomePage/TestShowWeather.java
@@ -1,22 +1,20 @@
 package testHomePage;
 
-import HomePage.PlayVideo;
+import HomePage.ShowWeather;
 import base.CommonAPI;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import utility.reporting.ApplicationLog;
-import utility.reporting.TestLogger;
 
 /**
  * Created by mehmet on 5/27/2017.
  */
-public class TestPlayVideo extends CommonAPI {
-  //  @Test
-    public void testPlayVideo() throws InterruptedException {
+public class TestShowWeather extends CommonAPI{
+    @Test
+    public void showWather(){
         ApplicationLog.epicLogger("Epic: Metro AC:1,2 <a https://peoplentch.atlassian.net/browse/GREEN-7> GREEN-7 </a>");
-        PlayVideo playButton = PageFactory.initElements(driver, PlayVideo.class);
-        playButton.playvideo();
-        }
+        ShowWeather weather = PageFactory.initElements(driver, ShowWeather.class);
+        weather.weatherForcast();
+
     }
-
-
+}
